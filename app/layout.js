bcfab76135540996104170953f0fdd6a2c70f7b3
Amr Waheed <amrmohamed877@gmail.com>
2025-05-04 18:50:29 +0300

    finishing setting up

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,21 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Josefin_Sans } from "next/font/google";
 import "./globals.css";
-import Navigation from "./components/Navigation";
-import Logo from "./components/Logo";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import Header from "@/app/_components/Header";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+
+const josefin = Josefin_Sans({
   subsets: ["latin"],
-});
+  display: "swap",
+})
+
 
 export const metadata = {
-  title: "The wild oasis",
-  description: "Generated ny Amr Waheed",
+  title: {
+    template: "%s - The Wild Oasis",
+    default: "Welcome - The Wild Oasis",
+  },
+  description: "The Wild Oasis is a unique and immersive experience that transports you to a world of tranquility and natural beauty. Nestled in the heart of nature, our oasis offers a serene escape from the hustle and bustle of everyday life. Whether you're seeking relaxation, adventure, or a blend of both, The Wild Oasis has something for everyone.",
 };
 
 export default function RootLayout({ children }) {
@@ -23,15 +23,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 flex flex-col min-h-screen`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>
-          {children}
-        </main>
+        <Header />
+        <div className="flex-1  px-8 py-12">
+          <main className="max-w-7xl mx-auto">
+            {children}
+          </main>
+        </div>
       </body>
     </html>
   );
